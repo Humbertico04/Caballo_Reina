ee4fac0181dd2374cb131338a5038a21c8d27a7b
Humbertico04 <humbertico2004@gmail.com>
2023-02-01 18:05:53 +0100

    sin optimizar2

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-a=[[4,6], [6,8], [7,9], [4,8], [3,9,0], [], [1,7,0], [2,6], [1,3], [2,4]]
-
+#Suma todos los elementos que hay
 def suma_elementos(lista):
     suma=0
     for i in range(len(lista)):
         suma+=len(lista[i])
     return suma
 
-def n_siguiente(lista):
+#Calcula los elementos de la siguiente lista en el árbol
+def rama_siguiente(lista):
     a=[[4,6], [6,8], [7,9], [4,8], [3,9,0], [], [1,7,0], [2,6], [1,3], [2,4]]
     b=[]
     for i in range(len(lista)):
@@ -14,14 +14,34 @@ def n_siguiente(lista):
             b.append(a[lista[i][j]])
     return b
 
-def n_algo(n):
+#Calcula los elementos del árbol hasta el n-ésimo nivel
+def n_ramas(n):
     a=[[4,6], [6,8], [7,9], [4,8], [3,9,0], [], [1,7,0], [2,6], [1,3], [2,4]]
-    for i in range(n-1):
-        b=n_siguiente(a)
-        print("{}:".format(i+2), suma_elementos(b))
+    for i in range(n):
+        b=rama_siguiente(a)
+        print(b)
+        print("{}:".format(i+1), suma_elementos(a))
         a=b
 
-n_algo(30)
+n_ramas(5)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # b=n_siguiente(a)
 # print(b)
 # print(suma_elementos(b))

cc5ce6d9ffe466446c6d076e7bb0055531fca3a3
Humbertico04 <humbertico2004@gmail.com>
2023-02-01 13:34:51 +0100

    segunda fase lista

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 a=[[4,6], [6,8], [7,9], [4,8], [3,9,0], [], [1,7,0], [2,6], [1,3], [2,4]]
 
-suma=0
-for i in range(len(a)):
-    suma+=len(a[i])
-print(suma)
+def suma_elementos(lista):
+    suma=0
+    for i in range(len(lista)):
+        suma+=len(lista[i])
+    return suma
 
-b=[]
-for i in range(len(a)):
-    for j in range(len(a[i])):
-        b.append(a[a[i][j]])
+def n_siguiente(lista):
+    b=[]
+    for i in range(len(lista)):
+        for j in range(len(lista[i])):
+            b.append(lista[lista[i][j]])
+    return b
+
+b=n_siguiente(a)
 print(b)
+print(suma_elementos(b))
